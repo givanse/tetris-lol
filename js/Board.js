ad661137c6f70428cf4df246e12c3627e8c00222
givanse <silvag777@gmail.com>
2013-08-01 14:24:55 -0500

    now we can draw falling shapes (squares)

--- js/Board.js
+++ js/Board.js
@@ -1,42 +1,61 @@
 
 function Board(canvasDiv, widthInSquares = 0, heightInSquares = 0) {
 
-  this.squaresMatrix = new SquaresMatrix(widthInSquares, heightInSquares);
+    this.squaresMatrix = new SquaresMatrix(widthInSquares, heightInSquares);
+    this.fallingSquares = new Array(4); /* Only 4 squares may fall at a time. */
+    this.insertFallingShape();
 
-  this.canvasDiv = canvasDiv; 
-  this.canvasDiv.setAttribute("style", "display: block; width: 0px; height: 0px");
-  /* canvasDiv dimensions */
-  var width  = widthInSquares  * SQUARE_SIZE;
-  var height = heightInSquares * SQUARE_SIZE;
-  /* set canvasDiv dimensions */
-  this.canvasDiv.style.width  = width  + "px";
-  this.canvasDiv.style.height = height + "px";
-
+    this.canvasDiv = canvasDiv; 
+    this.canvasDiv.setAttribute("style", 
+                                "display: block; width: 0px; height: 0px");
+    /* canvasDiv dimensions */
+    var width  = widthInSquares  * SQUARE_SIZE;
+    var height = heightInSquares * SQUARE_SIZE;
+    /* set canvasDiv dimensions */
+    this.canvasDiv.style.width  = width  + "px";
+    this.canvasDiv.style.height = height + "px";
 }
 
 Board.prototype.insertSquare = function(square) {
-  this.squaresMatrix.insertSquare(square);
+    this.squaresMatrix.insertSquare(square);
 }
 
 Board.prototype.getWidth = function() { 
-  return parseInt(this.canvasDiv.style.width); 
+    return parseInt(this.canvasDiv.style.width); 
 }
 
 Board.prototype.getHeight = function() { 
-  return parseInt(this.canvasDiv.style.height); 
+    return parseInt(this.canvasDiv.style.height); 
 }
 
+/**
+ * Draw, append, the squares to the canvas div.
+ */
 Board.prototype.drawSquares = function() { 
-  var squares = this.squaresMatrix.getSquares();
-  var width = this.squaresMatrix.getWidth();
-  var height =  this.squaresMatrix.getHeight();
-  for(var col = 0; col < width; col++) {
-    for(var row = 0; row < height; row++) {
-      var square = squares[col][row];
-      if(square)
-        this.canvasDiv.appendChild(square.getDiv());
+    /* draw board squares */
+    var squares = this.squaresMatrix.getSquares();
+    for(var col= 0; col < this.squaresMatrix.getWidth(); col++)
+        this.drawSquaresArray(squares[col]); 
+    /* draw falling squares */
+    this.drawSquaresArray(this.fallingSquares); 
+}
+
+Board.prototype.drawSquaresArray = function(squaresArray) { 
+    if(!(squaresArray instanceof Array))
+        return;
+
+    for(var i = 0; i < squaresArray.length; i++) {
+        var square = squaresArray[i];
+        if(square)
+            this.canvasDiv.appendChild(square.getDiv());
     }
-  }
+}
+
+Board.prototype.insertFallingShape = function() { 
+    this.fallingSquares[0] = new Square(4, 0);
+    this.fallingSquares[1] = new Square(5, 0);
+    this.fallingSquares[2] = new Square(4, 1);
+    this.fallingSquares[3] = new Square(5, 1);
 }
 
 /* EOF */
