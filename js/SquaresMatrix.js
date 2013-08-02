d06a786677d33c7c41a48314742fff6731f9ee77
givanse <silvag777@gmail.com>
2013-08-01 21:06:51 -0500

    SquaresMatrix.packColumn is ready

--- js/SquaresMatrix.js
+++ js/SquaresMatrix.js
@@ -1,23 +1,32 @@
 
-function SquaresMatrix(columns, rows) {                                            
+function SquaresMatrix(columns, rows) {
 
-    this.columns = columns < 0 ? 0 : columns;                                                 
-    this.rows = rows < 0 ? 0 : rows;                                              
+    this.columns = columns < 0 ? 0 : columns;
+    this.rows = rows < 0 ? 0 : rows;
 
-    this.squaresMatrix = new Array(this.columns);                                                 
-
-    for(var col = 0; col < this.columns; col++) {
-        this.squaresMatrix[col] = new Array(this.rows);
+    this.squaresMatrix = new Array(this.columns); 
+    for(var col = 0; col < this.columns; col++) { 
+        this.squaresMatrix[col] = new Array(this.rows); 
+        for(var row = 0; row < this.rows; row++) { 
+            this.squaresMatrix[col][row] = null; 
+        }
     }
-}          
+}
 
 SquaresMatrix.prototype.getWidth = function() { return this.columns; }
-                                                                                 
+
 SquaresMatrix.prototype.getHeight = function() { return this.rows; }
 
 SquaresMatrix.prototype.insertSquare = function(square) {
-    var x = square.getX(); 
-    var y = square.getY(); 
+    var x = square.getX();
+    var y = square.getY();
+    this.squaresMatrix[x][y] = square;
+    return this;
+}
+
+SquaresMatrix.prototype.insertSquareAt = function(x, y, square) {
+    square.setX(x);
+    square.setY(y);
     this.squaresMatrix[x][y] = square;
     return this;
 }
@@ -41,6 +50,25 @@ SquaresMatrix.prototype.arePositionsAvailable = function(positions) {
     }
 
     return true;
+}
+
+SquaresMatrix.prototype.packColumn = function(xConstant, y) {
+    for( ; y > 0; y--) { /* bottom to top */
+        var currentSquare = this.squaresMatrix[xConstant][y];
+        if(! (currentSquare instanceof Square)) {
+            var upperIndex = y;
+            var upperSquare;
+            do {
+                upperIndex--; /* reason for: for( y > 0) */
+                upperSquare = this.squaresMatrix[xConstant][upperIndex];
+            } while(! (upperSquare instanceof Square) && upperIndex > 0);
+
+            if(upperSquare instanceof Square) { /* found an upper Square? */
+                this.squaresMatrix[xConstant][upperIndex] = null;
+                this.insertSquareAt(xConstant, y, upperSquare);
+            }
+        }
+    }
 }
 
 /* EOF */
--- test/js/SquaresMatrixTest.js
+++ test/js/SquaresMatrixTest.js
@@ -24,7 +24,7 @@ test("Square.arePositionsAvailable", function() {
      * 0[x ]
      */
     var squaresMatrix = new SquaresMatrix(2, 1)
-                            .insertSquare(new Square(0, 0));
+                        .insertSquare(new Square(0, 0));
     var positions = [[1, 0]]; 
     ok(squaresMatrix.arePositionsAvailable(positions));
     var positions = [[0, 0], [1, 0]]; 
@@ -37,13 +37,119 @@ test("Square.arePositionsAvailable", function() {
      * 2[ ]
      */
     var squaresMatrix = new SquaresMatrix(1, 3)
-                            .insertSquare(new Square(0, 1));
+                        .insertSquare(new Square(0, 1));
     var positions = [[0, 1]]; 
     ok(!squaresMatrix.arePositionsAvailable(positions));
     var positions = [[0, 0], [0, 2]]; 
     ok(squaresMatrix.arePositionsAvailable(positions));
     var positions = [[0, 0], [0, 1], [0, 2]]; 
     ok(!squaresMatrix.arePositionsAvailable(positions));
+});
+
+test("Square.packColumn", function() {
+    /**
+     *   0
+     * 0[x]
+     * 1[ ]
+     */
+    var squaresMatrix = new SquaresMatrix(1, 2)
+                            .insertSquare(new Square(0, 0));
+    squaresMatrix.packColumn(0, 1); 
+    var expecteds = new SquaresMatrix(1, 2)                                  
+                        .insertSquare(new Square(0, 1)).getSquares();
+    var actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   0
+     * 0[ ]
+     * 1[x]
+     */
+    squaresMatrix = new SquaresMatrix(1, 2)
+                        .insertSquare(new Square(0, 1));
+    squaresMatrix.packColumn(0, 1); 
+    expecteds = new SquaresMatrix(1, 2)                                  
+                    .insertSquare(new Square(0, 1)).getSquares();
+    actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   0
+     * 0[x]
+     * 1[ ]
+     * 2[ ]
+     */
+    squaresMatrix = new SquaresMatrix(1, 3)
+                        .insertSquare(new Square(0, 0));
+    squaresMatrix.packColumn(0, 2); 
+    expecteds = new SquaresMatrix(1, 3)                                  
+                    .insertSquare(new Square(0, 2)).getSquares();
+    actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   0
+     * 0[ ]
+     * 1[x]
+     * 2[ ]
+     */
+    squaresMatrix = new SquaresMatrix(1, 3)
+                        .insertSquare(new Square(0, 1));
+    squaresMatrix.packColumn(0, 2); 
+    expecteds = new SquaresMatrix(1, 3)                                  
+                    .insertSquare(new Square(0, 2)).getSquares();
+    actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   0
+     * 0[x]
+     * 1[ ]
+     * 2[x]
+     */
+    squaresMatrix = new SquaresMatrix(1, 3)
+                        .insertSquare(new Square(0, 0, SQUARESHP))
+                        .insertSquare(new Square(0, 2, LINESHP));
+    squaresMatrix.packColumn(0, 2); 
+    expecteds = new SquaresMatrix(1, 3)                                  
+                    .insertSquare(new Square(0, 1, SQUARESHP))
+                    .insertSquare(new Square(0, 2, LINESHP)).getSquares();
+    actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   0
+     * 0[x]
+     * 1[ ]
+     * 2[ ]
+     * 3[x]
+     */
+    squaresMatrix = new SquaresMatrix(1, 4)
+                        .insertSquare(new Square(0, 0, SQUARESHP))
+                        .insertSquare(new Square(0, 3, LINESHP));
+    squaresMatrix.packColumn(0, 3); 
+    expecteds = new SquaresMatrix(1, 4)                                  
+                    .insertSquare(new Square(0, 2, SQUARESHP))
+                    .insertSquare(new Square(0, 3, LINESHP)).getSquares();
+    actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   0
+     * 0[x]
+     * 1[ ]
+     * 2[x]
+     * 3[ ]
+     */
+    squaresMatrix = new SquaresMatrix(1, 4)
+                        .insertSquare(new Square(0, 0, SQUARESHP))
+                        .insertSquare(new Square(0, 2, LINESHP));
+    squaresMatrix.packColumn(0, 2); 
+    expecteds = new SquaresMatrix(1, 4)                                  
+                    .insertSquare(new Square(0, 1, SQUARESHP))
+                    .insertSquare(new Square(0, 2, LINESHP)).getSquares();
+    actuals = squaresMatrix.getSquares();
+    deepEqual(actuals, expecteds);
 });
 
 /* EOF */
