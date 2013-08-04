3e995802fbbe174f81716f0d1a178a72ab1b1112
givanse <silvag777@gmail.com>
2013-08-04 13:26:53 -0500

    redefine base coordinates

--- test/js/GLOBALSTest.js
+++ test/js/GLOBALSTest.js
@@ -18,4 +18,72 @@ test("global variables", function() {
 
 });
 
+test("global variables", function() {
+
+    var actuals = TETROMINO_BASE_COORDINATES.LINESHP;
+    var expecteds = [[-1, 0], [0,0], [1, 0], [2, 0]];
+    deepEqual(actuals, expecteds, "LINESHP");
+
+    /**
+     *   -1 0 1
+     *  1 
+     *  0 x x
+     * -1 x x
+     */
+    actuals = TETROMINO_BASE_COORDINATES.SQUARESHP;
+    expecteds = [[-1, 0], [-1, -1], [0, 0], [0, -1]];
+    deepEqual(actuals, expecteds, "SQUARESHP");
+
+    /**
+     *   -1 0 1
+     *  1 
+     *  0 x x x
+     * -1   x
+     */
+    actuals = TETROMINO_BASE_COORDINATES.TSHP;
+    expecteds = [[-1, 0], [0, 0], [0, -1], [1, 0]];
+    deepEqual(actuals, expecteds, "TSHP");
+
+    /**
+     *   -1 0 1
+     *  1   x x 
+     *  0 x x 
+     * -1  
+     */
+    actuals = TETROMINO_BASE_COORDINATES.SSHP_R;
+    expecteds = [[-1, 0], [0, 1], [0, 0], [1, 0]];
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   -1 0 1
+     *  1 x x 
+     *  0   x x
+     * -1  
+     */
+    actuals = TETROMINO_BASE_COORDINATES.SSHP_L;
+    expecteds = [[-1, 1], [0, 1], [0, 0], [1, 0]];
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   -1 0 1
+     *  1 x
+     *  0 x x x
+     * -1  
+     */
+    actuals = TETROMINO_BASE_COORDINATES.LSHP_R;
+    expecteds = [[-1, 1], [-1, 0], [0, 0], [1, 0]];
+    deepEqual(actuals, expecteds);
+
+    /**
+     *   -1 0 1
+     *  1     x
+     *  0 x x x
+     * -1  
+     */
+    actuals = TETROMINO_BASE_COORDINATES.LSHP_L;
+    expecteds = [[-1, 0], [0, 0], [1, 1], [1, 0]];
+    deepEqual(actuals, expecteds);
+
+});
+
 /* EOF */
